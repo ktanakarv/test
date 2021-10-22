3dcf408d3e06fa0cb7eb17bd0fb5f46900121b7b
ktanakarv <ktanaka@redventures.com>
2021-10-22 10:57:35 -0400

    asdfa

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 circleci_files=`ls .circleci/*.txt`
-for file in $circleci_files
-if [ -s $file ]; then
-  echo "NOT EMPTY"
-  exit 1
-fi
+for file in $circleci_files; do
+  if [ -s $file ]; then
+    echo "NOT EMPTY"
+    exit 1
+  fi
+done
